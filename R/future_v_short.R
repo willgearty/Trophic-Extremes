059139f72b2f1baaf70e20a293f10262708f2720
Dr Rob Cooke <03rcooke@users.noreply.github.com>
2021-08-14 17:10:15 +0100

    Symmetric y axis

--- R/future_v_short.R
+++ R/future_v_short.R
@@ -30,6 +30,9 @@ chng <- yr_traj_c %>%
                 perc_low = ((low - med)/med) * 100,
                 perc_upp = ((upp - med)/med) * 100)
 
+# plot limit
+max_val <- max(c(abs(min(chng$perc_low)), abs(max(chng$perc_upp)))) * 1.01
+
 # Figure 5A ####
 # plot percent change in mass per trophic guild
 traj <- ggplot(chng, aes(x = yr, y = perc_med, colour = diet_5cat)) +
@@ -37,11 +40,10 @@ traj <- ggplot(chng, aes(x = yr, y = perc_med, colour = diet_5cat)) +
   geom_hline(aes(yintercept = 0, colour = diet_5cat), linetype = "dashed", lwd = 1) +
   geom_path(lwd = 1) +
   geom_ribbon(aes(ymin = perc_low, ymax = perc_upp, fill = diet_5cat), alpha = 0.4, colour = NA) +
+  scale_x_continuous(name = "Years in future", expand = c(0, 0)) +
+  scale_y_continuous(name = "Percent change in mass", limits = c(-max_val, max_val)) +
   scale_colour_manual(values = c("#359B73", "#2271B2", "#FFAC3B", "#CD022D")) +
   scale_fill_manual(values = c("#359B73", "#2271B2", "#FFAC3B", "#CD022D")) +
-  labs(x = "Years in future", y = "Percent change in mass") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(limits = c(-21, 21), expand = c(0,0)) +
   theme(legend.title = element_blank(),
         legend.position = "none",
         panel.spacing = unit(1.5, "lines"))
@@ -50,5 +52,5 @@ traj <- ggplot(chng, aes(x = yr, y = perc_med, colour = diet_5cat)) +
 traj_buff <- cowplot::plot_grid(traj, NULL, nrow = 1, rel_widths = c(1, 0.015))
 
 # save plot
-cowplot::save_plot("../figures/traj.pdf", traj_buff, base_height = 4, base_width = 10, dpi = 600)
+cowplot::save_plot("../figures/traj.pdf", traj_buff, base_height = 4, base_width = 9, dpi = 600)
 
